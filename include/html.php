f4d6e97c935bbd7fb004f1a2499df8551807bee5
amandahua17 <39344015+amandahua17@users.noreply.github.com>
2018-07-12 15:44:34 -0500

    Got a basic layout!

--- include/html.php
+++ include/html.php
@@ -5,10 +5,12 @@
 			<html>
 				<head>
 					<link href='https://fonts.googleapis.com/css?family=Roboto+Condensed' rel='stylesheet'>
+					<link href='https://fonts.googleapis.com/css?family=Roboto+Mono' rel='stylesheet'>
 					<title>$title</title>
 					<link rel='stylesheet' href='/style.css'>
 				</head>
 				<body>
+					<div id='home' class='fixed-bg home'>
 					<h1>Amanda Hua</h1>
 		";
 		navbar();
@@ -21,18 +23,14 @@
 
 	function navbar(){
 		echo"
-		<div class='navbar'>
-			<a href='#home'>home</a>	<a href='#'
-		</div>
+		<ul class='navbar'>
+			<li><a href='#home'>home</a></li>	<li><a href='#work'>work</a></li>	<li><a href='#education'>education</a></li>
+			<li><a href='#extracurriculars'>extracurriculars</a></li>	<li><a href='#music'>music</a></li>
+		</ul>
 		";
 	}
 
 	function foot(){
-		if($_SERVER['PHP_SELF']!= '/index.php'){
-			echo"
-					<br><a class='home' href='/index.php'>Home</a>
-			";
-		}
 		echo 		"<script src='/js/jquery.js'></script>
 					<script src='/js/jsfunctions.js'></script>
 				</body>
--- index.php
+++ index.php
@@ -5,6 +5,28 @@
 	head("Amanda Hua");
 		//content
 		echo"
+			</div>
+			<div id='work' class='fixed-bg work'>
+				<div id='workSection'>
+					<h2>Work</h2>
 
+					<a href='#home'>back to top</a>
+				</div>
+			</div>
+			<div id='education' class='fixed-bg education'>
+				<h2>Education</h2>
+
+				<a href='#home'>back to top</a>
+			</div>
+			<div id='extracurriculars' class='fixed-bg extracurriculars'>
+				<h2>Extracurriculars</h2>
+
+				<a href='#home'>back to top</a>
+			</div>
+			<div id='music' class='fixed-bg music'>
+				<h2>Music</h2>
+				
+				<a href='#home'>back to top</a>
+			</div>
 		";
 	foot();
