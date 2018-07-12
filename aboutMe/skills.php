3a886ce0aac46a182e8931c2727cee7570b12ed9
amandahua17 <39344015+amandahua17@users.noreply.github.com>
2018-07-12 11:31:58 -0500

    There's a bug where it's getting Yolo-Jar content

---
+++ aboutMe/skills.php
@@ -0,0 +1,5 @@
+<?php
+	include("/include/include_all.php");
+	head("Skills");
+	
+	foot();
--- include/html.php
+++ include/html.php
@@ -9,17 +9,23 @@
 					<link rel='stylesheet' href='/style.css'>
 				</head>
 				<body>
-		";
-		navbar();
-		echo"
 					<h1>$title</h1>
 		";
+		navbar();
 	}
 
 	function navbar(){
 		echo"
-			<div>
-
+		<div id='navbar'>
+			<div class='dropdown'>
+				<div onmouseover='showDrop()' onmouseout='hideDrop()' class='dropbtn'>About Me</div>
+				<div id='myDropdown' class='dropdown-content'>
+					<a href='/aboutMe/skills.php'>Skills</a>
+					<a href='/aboutMe/education.php'>Education</a>
+					<a href='/aboutMe/extra.php'>Extra</a>
+				</div>
+			</div>
+		</div>
 		";
 	}
 
